3c6cec9b9d6682dda776e1ac69316562cc129796
shivamgupta09 <shivam.gupta09@gmail.com>
2017-02-16 23:13:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,24 @@
-console.log('Loaded!');
+//console.log('Loaded!');
+var button = document.getElementById('counter');
+
+button.onclick = function() {
+    
+    var request = new XMLHttpRequest();
+
+    request.onreadystatechange = function() {
+        if(request.readystate === XMLHttpRequest.DONE)
+        {
+            if(request.status === 200)
+            {
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                 span.innerHTML = counter.toString();
+            }
+        }
+        
+    };
+        
+request.open('GET','http://shivamgupta09.imad.hasura-app.io/counter',true);
+request.send(null);
+    
+};
